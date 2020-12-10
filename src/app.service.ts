b0ec03cb8ef93432e63574dfdb750ec238afe854
Tin Truong <truongtrongtin0305@gmail.com>
2020-12-10 18:37:09 +0700

    Change root text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Read API document <a href="/api">here</>`;
   }
 }
