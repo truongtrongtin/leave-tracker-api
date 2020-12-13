8a4557516f08104e5cfd5b5674f1afa281cf8e49
Tin Truong <truongtrongtin0305@gmail.com>
2020-12-13 18:39:20 +0700

    First test cases

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,9 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe(
+        'Read API document <a href="/api">here</>',
+      );
     });
   });
 });
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,18 +1,39 @@
+import { EntityRepository } from '@mikro-orm/core';
+import { getRepositoryToken } from '@mikro-orm/nestjs';
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from 'users/user.entity';
+import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
+const mockUser = new User('test@gmail.com', '1234');
+const oneTask = new Task('test task 1', 'description 1', mockUser);
+
 describe('TasksService', () => {
-  let service: TasksService;
+  let tasksService: TasksService;
+  let taskRepository: EntityRepository<Task>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [
+        TasksService,
+        {
+          provide: getRepositoryToken(Task),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(oneTask),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<TasksService>(TasksService);
+    tasksService = module.get<TasksService>(TasksService);
+    taskRepository = module.get(getRepositoryToken(Task));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getTasks', () => {
+    it('should get a single task', () => {
+      const repoSpy = jest.spyOn(taskRepository, 'findOne');
+      expect(tasksService.getTaskById(1, mockUser)).resolves.toEqual(oneTask);
+      expect(repoSpy).toBeCalledWith({ id: 1, user: mockUser });
+    });
   });
 });
