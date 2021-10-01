69d9fb0b14b9314ee5e0146b6905b9eb93602b7a
Tin Truong <truongtrongtin0305@gmail.com>
2021-10-01 22:15:19 +0700

    fix(update user): also send new access token

--- src/leaves/leaves.module.ts
+++ src/leaves/leaves.module.ts
@@ -1,6 +1,6 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 import { AuthModule } from '../auth/auth.module';
 import { Leave } from './leave.entity';
 import { LeavesController } from './leaves.controller';
