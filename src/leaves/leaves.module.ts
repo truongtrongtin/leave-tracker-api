f5d03112cf72e858f738a43de2bbfc3f5d41ae13
Tin Truong <truongtrongtin0305@gmail.com>
2021-10-01 19:46:53 +0700

    chore(UsersModule): remove global

--- src/leaves/leaves.module.ts
+++ src/leaves/leaves.module.ts
@@ -1,12 +1,13 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
 import { AuthModule } from '../auth/auth.module';
 import { Leave } from './leave.entity';
 import { LeavesController } from './leaves.controller';
 import { LeavesService } from './leaves.service';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Leave]), AuthModule],
+  imports: [MikroOrmModule.forFeature([Leave]), AuthModule, UsersModule],
   controllers: [LeavesController],
   providers: [LeavesService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,10 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { forwardRef, Global, Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
-@Global()
 @Module({
   imports: [MikroOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
   controllers: [UsersController],
